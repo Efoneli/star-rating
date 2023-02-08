a2520d0f73362a5ab2845918f73eb4f7cbb0b980
Efoneli <abbaefoneli@gmail.com>
2023-02-09 07:21:37 +1030

    star

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
+import Star from "./Star";
+
+
 const StarRating = () => {
   // Initialize a 'courseRating' state
-
+    const [courseRating, setCourseRating] = useState(0)
   // Write a function that returns 5 Star components
-  
+  const renderStars = () => {
+    const stars = []
+    const maxStars = 5
+    for (let i = 0; i < maxStars; i++) {
+        stars.push(
+            <Star 
+            key = {i}
+            setRating =  {() => handleCourseRating()}
+            // isSelected = {}
+            />
+        ); 
+    }
+    return stars;
+  }
   // Write an event handler that updates the courseRating state.
+  const handleCourseRating = (event) => {
+    setCourseRating(event)
+  }
   // Pass the function to a Star component via props
   
   return (
     <ul className="course--stars">
       {/* Render the Star components */}
+      {renderStars()}
     </ul>
   );
 };
